cfd292710fb1b8ab856215b671c04b6a86e7ae39
Prabhu Subramanian <prabhu@appthreat.com>
2026-04-16 23:40:49 +0100

    bug fix
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- types/lib/helpers/protobom.d.ts
+++ types/lib/helpers/protobom.d.ts
@@ -1,5 +1,5 @@
 export function writeBinary(bomJson: string | Object, binFile: string): void;
-export function readBinary(binFile: string, asJson?: boolean, specVersion?: number): cdx_16.Bom | cdx_15.Bom | import("@bufbuild/protobuf").JsonValue | undefined;
+export function readBinary(binFile: string, asJson?: boolean, specVersion?: number): cdx_16.Bom | import("@bufbuild/protobuf").JsonValue | cdx_17.Bom | undefined;
 import { cdx_16 } from "@appthreat/cdx-proto";
-import { cdx_15 } from "@appthreat/cdx-proto";
+import { cdx_17 } from "@appthreat/cdx-proto";
 //# sourceMappingURL=protobom.d.ts.map
