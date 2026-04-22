3f5edf8adb67c5b3bff112291af11ecc72ad3e96
Prabhu Subramanian <prabhu@appthreat.com>
2026-04-22 02:25:24 +0100

    feat: add end() method to table stream for proper bottom border rendering

--- types/lib/helpers/table.d.ts
+++ types/lib/helpers/table.d.ts
@@ -1,5 +1,6 @@
 export function table(rows: any, config?: {}): string;
 export function createStream(config?: {}): {
     write(row: any): void;
+    end(): void;
 };
 //# sourceMappingURL=table.d.ts.map
