8b389c95038db530d67a2b24b965974b8016b3b1
Prabhu Subramanian <prabhu@appthreat.com>
2026-05-13 15:35:01 +0100

    types
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

---
+++ types/lib/helpers/hbomLoader.d.ts
@@ -0,0 +1,8 @@
+/**
+ * Resolve the cdx-hbom module, preferring the local workspace checkout when the
+ * installed dependency does not yet expose the newer trace API.
+ *
+ * @returns {Promise<object>} Loaded cdx-hbom module namespace.
+ */
+export function importHbomModule(): Promise<object>;
+//# sourceMappingURL=hbomLoader.d.ts.map
