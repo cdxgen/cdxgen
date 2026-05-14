fbf26bf5f7325cb8085c06f2c7f8208a8e834612
Prabhu Subramanian <prabhu@appthreat.com>
2026-05-14 23:31:45 +0100

    types
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- types/lib/helpers/protobom.d.ts
+++ types/lib/helpers/protobom.d.ts
@@ -1,4 +1,3 @@
-export function isProtoBomFile(filePath: string): boolean;
-export function writeBinary(bomJson: string | Object, binFile: string, specVersion?: string | number): void;
-export function readBinary(binFile: string, asJson: boolean, specVersion?: string | number): any;
+export function writeBinary(bomJson: string | any, binFile: string): void;
+export function readBinary(binFile: string, asJson?: boolean, specVersion?: number): any;
 //# sourceMappingURL=protobom.d.ts.map
