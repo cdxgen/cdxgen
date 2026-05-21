30edcc3e64de5387eca96b5c2fe66dbae8d4c2f8
Prabhu Subramanian <prabhu@appthreat.com>
2026-05-21 17:36:15 +0530

    types
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

---
+++ types/lib/helpers/dosaiParsers.d.ts
@@ -0,0 +1,8 @@
+export function normalizeDosaiPurlKey(purl: any): string | undefined;
+export function addDosaiSetValue(map: any, key: any, value: any): void;
+export function dosaiLocation(item: any): any;
+export function dosaiSourceLocationFromNode(node: any): any;
+export function dosaiSourceLocation(location: any): any;
+export function buildDosaiPurlAliasMap(components?: any[]): Map<any, any>;
+export function resolveDosaiComponentPurl(purl: any, purlAliasMap: any): any;
+//# sourceMappingURL=dosaiParsers.d.ts.map
