bb9a78ace9f5a3c4adcd6c7e4bda4dfea11a0d6c
Prabhu Subramanian <prabhu@appthreat.com>
2026-05-26 21:05:35 +0530

    types
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

---
+++ types/lib/helpers/golem.d.ts
@@ -0,0 +1,13 @@
+export function isGolemGoLanguage(language: any): boolean;
+export function readGolemJsonFile(jsonFile: any): any;
+export function runGolemAnalysis(src: any, outputFile: any, options?: {}): boolean;
+export function analyzeGolemProject(src: any, options?: {}): any;
+export function collectGolemEvidence(golemReport?: {}, components?: any[]): {
+    componentPropertiesMap: {};
+    cryptoComponents: any[];
+    cryptoGeneratePurls: {};
+    dataFlowFrames: {};
+    metadataProperties: any[];
+    purlLocationMap: {};
+};
+//# sourceMappingURL=golem.d.ts.map
